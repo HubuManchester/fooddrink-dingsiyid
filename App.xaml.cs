d476a158b6189e99d2ac0908c8356ad33aac883f
dingsiyid <18186140843@163.com>
2026-05-22 09:08:26 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CampusEats
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
